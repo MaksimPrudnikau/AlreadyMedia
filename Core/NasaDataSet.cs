3b9338a8f06de8ce4f01057f7987d51ac1c6f567
Maksim Prudnikau <60088883+MaksimPrudnikau@users.noreply.github.com>
2025-07-24 16:59:50 +0300

    init Core lib with NasaDataSet

---
+++ Core/NasaDataSet.cs
@@ -0,0 +1,21 @@
+﻿namespace Core;
+
+public class NasaDataSet
+{
+    public string Id { get; set; }
+    
+    public string? Center { get; set; }
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public string? Photographer { get; set; }
+    public string? Location { get; set; }
+    public string? KeywordsJson { get; set; }
+    
+    public DateTime? DateCreated { get; set; }
+    
+    public string? MediaType { get; set; }
+    public string? ThumbnailUrl { get; set; }
+    public string? FullResUrl { get; set; }
+    
+    public DateTime LastUpdated { get; set; } 
+}

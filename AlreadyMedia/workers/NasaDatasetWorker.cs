8423505e4624624e00542a5e41cc8e394ce053a7
Maksim Prudnikau <60088883+MaksimPrudnikau@users.noreply.github.com>
2025-07-24 17:01:10 +0300

    init worker

---
+++ AlreadyMedia/workers/NasaDatasetWorker.cs
@@ -0,0 +1,9 @@
+namespace AlreadyMedia.workers;
+
+public class NasaDatasetWorker: BackgroundService
+{
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        throw new NotImplementedException();
+    }
+}

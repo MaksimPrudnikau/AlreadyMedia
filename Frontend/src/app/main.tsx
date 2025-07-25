fafe230b05f0c2a3e5a224b6d2ec4fe4ccccbaae
Maksim Prudnikau <60088883+MaksimPrudnikau@users.noreply.github.com>
2025-07-25 19:06:42 +0300

    refactor to FSD

--- Frontend/src/app/main.tsx
+++ Frontend/src/app/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./App.css";
-import App from "./App.tsx";
+import "@/app/styles/App.css";
+import { IndexPage } from "@/pages/index";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <IndexPage />
   </StrictMode>,
 );

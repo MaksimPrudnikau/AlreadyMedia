4fa9eace8fa30359924e3ba4c4c4302c02322996
Maksim Prudnikau <60088883+MaksimPrudnikau@users.noreply.github.com>
2025-07-25 12:59:58 +0300

    add filters validation

--- Core/Models/NasaDatasetListRequest.cs
+++ Core/Models/NasaDatasetListRequest.cs
@@ -1,20 +1,36 @@
 using System.ComponentModel.DataAnnotations;
-using System.Runtime.CompilerServices;
 
 namespace Core.Models;
 
-public class NasaDatasetListRequest
+public class NasaDatasetListRequest: IValidatableObject
 {
-    [Required]
-    [Range(2000, 2500)]
+    [Display(Name = "From Year")]
+    [Range(1, 9999, ErrorMessage = "The {0} must be between {1} and {2}.")]
     public int? FromYear { get; init; }
     
-    
+    [Display(Name = "To Year")]
+    [Range(1, 9999, ErrorMessage = "The {0} must be between {1} and {2}.")]
     public int? ToYear { get; init; }
     
-    [Required]
+    [Display(Name = "Record Class")]
+    [StringLength(50, ErrorMessage = "The {0} cannot exceed {1} characters.")]
     public string? RecClass { get; init; }
-
+    
+    [Display(Name = "Page Number")]
+    [Range(0, int.MaxValue, ErrorMessage = "The {0} cannot be negative.")]
     public int Page { get; init; } = 0;
+    
+    [Display(Name = "Items Per Page")]
+    [Range(1, 10, ErrorMessage = "The {0} must be between {1} and {2}.")]
     public int ItemsPerPage { get; init; } = 10;
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (FromYear > ToYear)
+        {
+            yield return new(
+                "Selected years are in reverse order.",
+                [nameof(FromYear), nameof(ToYear)]);
+        }
+    }
 }
